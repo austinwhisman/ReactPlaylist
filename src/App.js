7f1b9991347ffd746e49e96164eb85e09c3f7741
austin whisman <austinlwhisman@gmail.com>
2018-01-19 13:18:40 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+let defaultStyle = {
+  color: '#fff'
+}
+class Aggregate extends Component {
+  render(){
+    return (
+      <div style={{width: '40%', display: 'inline-block'}}>
+        <h2 style={defaultStyle}>Number Text</h2>
+      </div>
+    );
+  }
+}
+
+
+class Filter extends Component {
+  render(){
+    return(
+      <div style={defaultStyle}>
+        <img/>
+        <input type="text"/>
+
+      </div>
+    )
+  }
+}
+
+class Playlist extends Component {
+  render() {
+    return(
+      <div style ={{...defaultStyle, width: "25%", display: "inline-block"}}>
+      <img/>
+      <h3>Playlist Name</h3>
+      <ul>
+        <li>Song 1</li>
+        <li>Song 2</li>
+        <li>Song 3</li>
+      </ul>
+      </div>
+    )
+  }
+}
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1 style ={{...defaultStyle, fontsize:'54px'}}>Title</h1>
+        <Aggregate/>
+        <Aggregate/>
+        <Filter/>
+        <Playlist/>
+        <Playlist/>
+        <Playlist/>
+      </div>
+    );
+  }
+}
+
+export default App;
